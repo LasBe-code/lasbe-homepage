c18ec69a110087a60eb19705eb65533857550662
lasbe <devlasbe@gmail.com>
2024-10-06 22:38:03 +0900

    refactor: divide Layout Component in AboutMeModal

---
+++ app/_components/aboutMeModal/Layout.tsx
@@ -0,0 +1,28 @@
+type LayoutType = {
+  title: string;
+  headerColorClassName: string;
+  isUseBgColor?: boolean;
+  children: React.ReactElement;
+};
+
+export default function ({
+  title,
+  headerColorClassName,
+  isUseBgColor = true,
+  children,
+}: LayoutType) {
+  return (
+    <div
+      className={`relative flex flex-col gap-8 items-center ${
+        isUseBgColor ? "py-12 bg-white shadow-lg" : ""
+      } rounded-lg`}
+    >
+      <h1
+        className={`z-10 absolute top-[-20px] px-4 py-1 rounded-full shadow-[0_2px_4px_0px_rgba(0,0,0,0.4)] ${headerColorClassName} text-white font-extrabold text-2xl`}
+      >
+        {title}
+      </h1>
+      {children}
+    </div>
+  );
+}
