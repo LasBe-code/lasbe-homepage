f0965548c2ff91b9dfc872afe7d4201b43b9ed30
lasbe <devlasbe@gmail.com>
2024-10-06 22:38:25 +0900

    refactor: divide Typing Component in MainPage

---
+++ app/_components/Typing.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState, useRef, useEffect } from "react";
+
+export default function ({ text }: { text: string }) {
+  const [result, setResult] = useState(text.charAt(0));
+  const [index, setIndex] = useState(1);
+  const timerRef = useRef(false);
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      if (index === text.length && !timerRef.current) {
+        setTimeout(() => {
+          setResult(text.charAt(0));
+          setIndex(1);
+          timerRef.current = false;
+        }, 1000);
+        timerRef.current = true;
+      }
+      if (index < text.length) {
+        setResult(result + text.charAt(index));
+        setIndex(index + 1);
+      }
+    }, 150);
+    return () => clearInterval(interval);
+  }, [result, index]);
+
+  return (
+    <div
+      className={`font-archivo text-md md:text-2xl font-extrabold text-wrap whitespace-pre-wrap text-center leading-normal`}
+    >
+      <span>{result}</span>
+      <span className="cursor-animation border-r-2 px-0.5 border-neutral-900" />
+    </div>
+  );
+}
