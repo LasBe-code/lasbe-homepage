42e02868f00c86c46843222e7f2f6cce6a830b00
lasbe <devlasbe@gmail.com>
2024-10-18 02:10:38 +0900

    fix: date month 값 오차 수정

--- utils/date.ts
+++ utils/date.ts
@@ -1,5 +1,7 @@
 const current = new Date();
-const todayDate = `${current.getFullYear()}-${current.getMonth()}-${current.getDay()}`;
+const todayDate = `${current.getFullYear()}-${
+  current.getMonth() + 1
+}-${current.getDay()}`;
 const calcBetweenDate = (start: string, end: string) => {
   const diffInTime = new Date(end).getTime() - new Date(start).getTime();
   const diffInDays = diffInTime / (1000 * 60 * 60 * 24);
